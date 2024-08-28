f7653b8d7db970a2525f79827514fd37c2526ac8
Abiny Magalhães <162757097+Mikkezinn@users.noreply.github.com>
2024-08-27 21:38:09 -0300

    App.js

--- App.js
+++ App.js
@@ -5,17 +5,20 @@ import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
 export default function App() {
   const [email, setEmail] = useState('')
-  const [senha, setSenha ] = useState('')
+  const [senha, setSenha ] = useState('') 
+  const [resultado, setResultado ] = useState(true) //boleano
   const ValidaLogin = function (){
     if (email == 'admin@gmail.com' && senha == '1234') {
+      setResultado (true)
       console.log ('Login Correto')
     } else {
+      setResultado(false)
 console.log ('Incorreto')
-
-    }
+ }
   }   
   return (
     <View style={styles.container}>
+      {resultado?null:<Text>Usuario errou email ou senha!</Text>}
       <TextInput placeholder='Email' onChangeText={(text)=>setEmail(text)} />
       <TextInput placeholder='Senha' onChangeText={(text)=>setSenha(text)} />
         <Button title='Entrar' onPress={()=> ValidaLogin()} />
